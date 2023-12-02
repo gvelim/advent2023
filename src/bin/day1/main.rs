7c403b6d84d75f111e84af209be82c5bd6a3cc7c
gvelim <3185416+gvelim@users.noreply.github.com>
2023-12-02 23:57:52 +0000

    Day 1 - part2 - extract_first_last_part2()   - remove match   - DIGITS into (key,value) - refactor names

--- src/bin/day1/main.rs
+++ src/bin/day1/main.rs
@@ -1,24 +1,19 @@
-use std::collections::HashMap;
-
-static DIGITS: [&str; 9] = [
-    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
-];
-
-static HMDIGITS: HashMap<&str,u32> = HashMap::from([
-    ("one",1), ("two",2), ("three",3), ("four",4), ("five",5), ("six",6), ("seven",7), ("eight",8), ("nine",9)
-]);
-
-
 fn main() {
     let inp = std::fs::read_to_string("src/bin/day1/input.txt").unwrap_or_else(|e| panic!("{e}"));
 
     let sum = inp.lines()
-        .filter_map(|line| extract_first_last(line))
+        .filter_map(|line| extract_first_last_part1(line))
         .sum::<u32>();
-    println!("Sum = {sum}");
+    println!("Part 1 -> Sum = {sum}");
+
+    let sum = inp.lines()
+        .filter_map(|line| extract_first_last_part2(line))
+        .inspect(|num| println!("Result: {num}"))
+        .sum::<u32>();
+    println!("Part 2 -> Sum = {sum}");
 }
 
-fn extract_first_last(inp: &str) -> Option<u32> {
+fn extract_first_last_part1(inp: &str) -> Option<u32> {
     use std::ops::Not;
 
     let mut tmp = vec![];
@@ -34,17 +29,23 @@ fn extract_first_last(inp: &str) -> Option<u32> {
 
 fn extract_first_last_part2(input: &str) -> Option<u32> {
     use std::ops::Not;
+    static DIGITS: [(&str,u32); 9] = [
+        ("one",1), ("two",2), ("three",3), ("four",4), ("five",5), ("six",6), ("seven",7), ("eight",8), ("nine",9)
+    ];
 
     // String Buffer to store non-numeric chars for lateral processing
     let mut buf = String::new();
-
+    print!("Inp: {input} -> ");
     // for every char in the input string
     let tmp = input
         .chars()
         .filter_map(|c| {
             match c {
                 // if digit convert to numeric
-                '1'..='9' => c.to_digit(10),
+                '1'..='9' => {
+                    buf.clear();
+                    c.to_digit(10)
+                },
                 // if non-digit
                 'a'..='z' => {
                     // push into the string
@@ -53,26 +54,15 @@ fn extract_first_last_part2(input: &str) -> Option<u32> {
                     DIGITS
                         .iter()
                         // check if the string we have in BUF matches any of the DIGIT names
-                        .filter_map(|d| {
+                        .filter_map(|(d,numeric)| {
                             // if BUF doesn't match any return NONE
                             if !buf.ends_with(d) { None }
                             else {
                                 // we have a match
-                                println!("BUF:{buf}");
+                                print!("{buf},");
                                 // clear the buffer
                                 buf.clear();
-                                match d {
-                                    &"one" => Some(1),
-                                    &"two" => Some(2),
-                                    &"three" => Some(3),
-                                    &"four" => Some(4),
-                                    &"five" => Some(5),
-                                    &"six" => Some(6),
-                                    &"seven" => Some(7),
-                                    &"eight" => Some(8),
-                                    &"nine" => Some(9),
-                                    _ => None
-                                }
+                                Some(*numeric)
                             }
                         })
                         .next()
@@ -82,6 +72,7 @@ fn extract_first_last_part2(input: &str) -> Option<u32> {
         })
         .collect::<Vec<_>>();
 
+    print!("{:?}!! ",tmp);
     tmp.is_empty()
         .not()
         .then(|| 10 * tmp.first().unwrap() + tmp.last().unwrap() )
@@ -101,20 +92,19 @@ mod test {
     ];
 
     #[test]
-    fn test_extract_digit() {
+    fn test_extract_digit_numeric() {
         INPUT.iter()
             .for_each(|str|{
                 print!("{str:?} : ");
-                println!(" -> {:?}",extract_first_last(str))
+                println!(" -> {:?}", extract_first_last_part1(str))
             })
     }
 
     #[test]
-    fn test_string_in_string() {
+    fn test_extract_digit_word() {
 
         INPUT.iter()
             .for_each(|input| {
-                print!("Inp: {input}");
                 println!("Found: {:?}", extract_first_last_part2(input) );
             });
         assert!(true)
