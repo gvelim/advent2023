4f2a12b8ee1c4cbb04653c337e271e5664502973
gvelim <3185416+gvelim@users.noreply.github.com>
2023-12-02 14:08:16 +0000

    Day 1 - part2 - WIP

--- src/bin/day1/main.rs
+++ src/bin/day1/main.rs
@@ -1,4 +1,9 @@
 
+
+static DIGITS: [&str; 9] = [
+    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
+];
+
 fn main() {
     let inp = std::fs::read_to_string("src/bin/day1/input.txt").unwrap_or_else(|e| panic!("{e}"));
 
@@ -25,7 +30,16 @@ fn extract_first_last(inp: &str) -> Option<u32> {
 #[cfg(test)]
 mod test {
     use super::*;
-    static INPUT: [&str; 4] = ["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f","treb7uchet"];
+    static INPUT: [&str; 7] = [
+        "two1nine",
+        "eightwothree",
+        "abcone2threexyz",
+        "xtwone3four",
+        "4nineeightseven2",
+        "zoneight234",
+        "7pqrstsixteen"
+    ];
+
     #[test]
     fn test_extract_digit() {
         INPUT.iter()
@@ -33,5 +47,34 @@ mod test {
                 print!("{str:?} : ");
                 println!(" -> {:?}",extract_first_last(str))
             })
+    }
+
+    #[test]
+    fn test_string_in_string() {
+
+        INPUT.iter()
+            .for_each(|inp|{
+                let mut i = 0;
+
+                let itr = inp.chars();
+
+                println!("INP: {inp}");
+
+                while i < inp.len()-1 {
+                    DIGITS.iter()
+                        .any(|digit| {
+                            if !inp[i..].starts_with(digit) { false }
+                            else {
+                                println!("{i}:{}",&inp[i..i+digit.len()],);
+                                i += digit.len()-1;
+                                true
+                            }
+                        });
+                    i += 1;
+                }
+            });
+
+
+        assert!(true)
     }
 }
