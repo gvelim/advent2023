f1e5513291e65b272cf66ac3f04be4fb9802697a
gvelim <3185416+gvelim@users.noreply.github.com>
2023-12-14 20:35:18 +0000

    Day8 - refactor - MapIter constructor, drop HashMap and use reference to Network object instead

--- src/bin/day8/network.rs
+++ src/bin/day8/network.rs
@@ -7,12 +7,21 @@ pub(crate) struct Network<'a> {
 
 impl Network<'_> {
 
-    pub(crate) fn iter<'a>(&'a self, start: &'a str, turns: impl Iterator<Item=char>) -> NetworkIter<'a, impl Iterator<Item=char>> {
-        NetworkIter { net: &self.net, start, turns }
+    pub(crate) fn iter<'a>(
+        &'a self,
+        start: &'a str,
+        turns: impl Iterator<Item=char>
+    ) -> NetworkIter<'a, impl Iterator<Item=char>> {
+        NetworkIter { net: &self, start, turns }
     }
 
-    pub(crate) fn par_iter<'a>(&'a self, start: &'a [&'a str], turns: impl Iterator<Item=char>) -> ParNetworkIter<'a, impl Iterator<Item=char>> {
-        ParNetworkIter { net: &self.net, turns,
+    pub(crate) fn par_iter<'a>(
+        &'a self,
+        start: &'a [&'a str],
+        turns: impl Iterator<Item=char>
+    ) -> ParNetworkIter<'a, impl Iterator<Item=char>> {
+        ParNetworkIter {
+            net: &self, turns,
             start: start.to_vec()
         }
     }
@@ -31,7 +40,7 @@ impl Network<'_> {
 }
 
 pub(crate) struct NetworkIter<'a,I> where I: Iterator<Item=char> {
-    net: &'a HashMap<&'a str,(&'a str,&'a str)>,
+    net: &'a Network<'a>,
     start: &'a str,
     turns: I
 }
@@ -41,8 +50,8 @@ impl<'a, I> Iterator for NetworkIter<'a, I> where I: Iterator<Item=char> {
 
     fn next(&mut self) -> Option<Self::Item> {
         match self.turns.next() {
-            Some('L') => self.net.get(self.start).map(|(l,_)| *l),
-            Some('R') => self.net.get(self.start).map(|(_,r)| *r),
+            Some('L') => self.net.net.get(self.start).map(|(l,_)| *l),
+            Some('R') => self.net.net.get(self.start).map(|(_,r)| *r),
             _ => unreachable!()
         }.map(|next| {
             self.start = next;
@@ -52,7 +61,7 @@ impl<'a, I> Iterator for NetworkIter<'a, I> where I: Iterator<Item=char> {
 }
 
 pub(crate) struct ParNetworkIter<'a,I> where I: Iterator<Item=char> {
-    net: &'a HashMap<&'a str,(&'a str,&'a str)>,
+    net: &'a Network<'a>,
     start: Vec<&'a str>,
     turns: I
 }
@@ -66,8 +75,8 @@ impl<'a, I> Iterator for ParNetworkIter<'a, I> where I: Iterator<Item=char> {
             .map(|node|
                 match turn {
                     None => unreachable!(),
-                    Some('L') => self.net.get(node).map(|(l,_)| *l).unwrap(),
-                    Some('R') => self.net.get(node).map(|(_,r)| *r).unwrap(),
+                    Some('L') => self.net.net.get(node).map(|(l,_)| *l).unwrap(),
+                    Some('R') => self.net.net.get(node).map(|(_,r)| *r).unwrap(),
                     _ => unreachable!()
                 }
             )
