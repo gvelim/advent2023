53ddf6ec7b88e830288c4177a8bbdba042967728
george velimachitis <3185416+gvelim@users.noreply.github.com>
2023-12-14 16:40:42 +0000

    Day 8 - Part 2 WIP Drying to figure LCM way

--- src/bin/day8/main.rs
+++ src/bin/day8/main.rs
@@ -9,26 +9,40 @@ use crate::{
 fn main() {
     let input = std::fs::read_to_string("./src/bin/day8/input.txt").expect("Ops!");
     let split = input.split("\n\n").next().unwrap();
+    let turns_len = split.len();
     let mut turns = Directions::parse(split);
     let mut net = Network::parse(input.as_str());
 
-    let count = net.iter("AAA", &mut turns)
-        .inspect(|n| print!("{:?},",n))
-        .take_while(|node| node.ne(&"ZZZ") )
-        .count() + 1;
+    let mut run_part = |start: &str, net: &mut Network, cmp: fn(&str) -> bool| {
+        net.iter(start, &mut turns)
+            // .inspect(|n| print!("{:?},",n))
+            .take_while(|node| cmp(node))
+            .count() + 1
+    };
 
-    println!("\nPart 1: Count {}",count);
+    println!("\nPart 1: Count {:?}",
+             run_part("AAA", &mut net, |n| !n.eq("ZZZ"))
+    );
 
-    let a_nodes = net.net.keys().filter(|s| s.ends_with('A')).copied().collect::<Vec<_>>();
-    println!("{:?}",a_nodes);
-    let count = net.par_iter(&a_nodes, turns)
-        // .inspect(|n| println!("{:?}",n))
-        .take_while(|nodes|{
-            !nodes.iter().all(|node| node.ends_with("Z"))
-        })
-        .count() + 1;
+    let mut a_nodes = net.net.keys().filter(|s| s.ends_with('A')).copied().collect::<Vec<_>>();
+    a_nodes.sort();
+    println!("{:?}",(&a_nodes,&turns_len));
 
-    println!("\nPart 2: Count {}",count);
+    a_nodes.iter()
+        .inspect(|n| print!("{:?} -> ",n))
+        .for_each(|node| {
+            let sum = run_part(node, &mut net, |n| !n.ends_with('Z'));
+            println!("Part 2: Count {:?}", sum)
+        });
+
+    // let count = net.par_iter(&a_nodes, turns)
+    //     // .inspect(|n| println!("{:?}",n))
+    //     .take_while(|nodes|{
+    //         !nodes.iter().all(|node| node.ends_with("Z"))
+    //     })
+    //     .count() + 1;
+    //
+    // println!("\nPart 2: Count {}",count);
 }
 
 #[cfg(test)]
@@ -39,14 +53,44 @@ mod test {
 
     static INPUT_P1: &str = "LLR\n\nAAA = (BBB, BBB)\nBBB = (AAA, ZZZ)\nZZZ = (ZZZ, ZZZ)";
     static INPUT_P2: &str = "LR\n\n11A = (11B, XXX)\n11B = (XXX, 11Z)\n11Z = (11B, XXX)\n22A = (22B, XXX)\n22B = (22C, 22C)\n22C = (22Z, 22Z)\n22Z = (22B, 22B)\nXXX = (XXX, XXX)";
+
+    #[test]
+    fn test_network_lcm() {
+        let mut split = INPUT_P2.split("\n\n");
+        let mut turns = Directions::parse(split.next().unwrap());
+        let mut net = Network::parse(INPUT_P2);
+
+        let mut run_part = |start: &str, net: &mut Network, cmp: fn(&str) -> bool| {
+            net.iter(start, &mut turns)
+                // .inspect(|n| print!("{:?},",n))
+                .take_while(|node| cmp(node))
+                .count() + 1
+        };
+
+        let mut a_nodes = net.net.keys().filter(|s| s.ends_with('A')).copied().collect::<Vec<_>>();
+        a_nodes.sort();
+        println!("{:?}",a_nodes);
+
+        a_nodes.iter()
+            .inspect(|n| print!("{:?} -> ",n))
+            .for_each(|node| {
+                let sum = run_part(node, &mut net, |n| !n.ends_with('Z'));
+                println!("Part 2: Count {:?}", sum)
+            });
+
+        assert_eq!(6,6)
+    }
+
     #[test]
     fn test_network_parallel_traversing() {
         let mut split = INPUT_P2.split("\n\n");
         let turns = Directions::parse(split.next().unwrap());
         let mut net = Network::parse(INPUT_P2);
 
-        let a_nodes = net.net.keys().filter(|s| s.ends_with('A')).copied().collect::<Vec<_>>();
+        let mut a_nodes = net.net.keys().filter(|s| s.ends_with('A')).copied().collect::<Vec<_>>();
+        a_nodes.sort();
         println!("{:?}",a_nodes);
+
         let count = net.par_iter(&a_nodes, turns)
             .inspect(|n| println!("{:?}",n))
             .take_while(|nodes|
