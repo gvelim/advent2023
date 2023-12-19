191ad625d5d08d176ab4f230ec9888d72a1b83ac
gvelim <3185416+gvelim@users.noreply.github.com>
2023-12-19 12:37:43 +0000

    Day 10 - refactor code restructure

---
+++ src/bin/day10/elf.rs
@@ -0,0 +1,55 @@
+use crate::{
+    field::Field,
+    direction::{
+        Direction,
+        Direction::{Down, Left, Right, Up}
+    }
+};
+
+#[derive(Debug,PartialEq)]
+pub(crate) struct Elf<'a> {
+    pub(crate) field: &'a Field,
+    pub(crate) pos: (usize, usize),
+    pub(crate) dir: Direction
+}
+
+impl Elf<'_> {
+    pub(crate) fn possible_directions(&self) -> Vec<Direction> {
+        vec![
+            self.field.get_pipe((self.pos.0-1,self.pos.1)).and_then(|p| Left.pipe_exit(p)),
+            self.field.get_pipe((self.pos.0+1,self.pos.1)).and_then(|p| Right.pipe_exit(p)),
+            self.field.get_pipe((self.pos.0,self.pos.1-1)).and_then(|p| Up.pipe_exit(p)),
+            self.field.get_pipe((self.pos.0,self.pos.1+1)).and_then(|p| Down.pipe_exit(p))
+        ].iter()
+            .filter_map(|&d| d)
+            .collect::<Vec<_>>()
+    }
+    fn step_one(&mut self) -> Option<char> {
+        let pos = match self.dir {
+            Up => (self.pos.0, self.pos.1-1),
+            Right => (self.pos.0+1, self.pos.1),
+            Down => (self.pos.0, self.pos.1+1),
+            Left => (self.pos.0-1, self.pos.1),
+        };
+        // have we landed on a valid position ?
+        self.field.get_pipe(pos)
+            .and_then(|p|
+                // Can we enter the new pipe from current direction ?
+                self.dir.pipe_exit(p)
+                    // new pipe is connected to current hence move one step
+                    .and_then(|dir| {
+                        self.pos = pos;
+                        self.dir = dir;
+                        Some(p)
+                    })
+            )
+    }
+}
+
+impl Iterator for Elf<'_> {
+    type Item = char;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.step_one()
+    }
+}
---
+++ src/bin/day10/field.rs
@@ -0,0 +1,42 @@
+use crate::direction::Direction::*;
+use crate::elf::Elf;
+
+#[derive(Debug,PartialEq)]
+pub(crate) struct Field {
+    pub(crate) data: Vec<char>,
+    pub(crate) width: usize,
+    pub(crate) start: (usize, usize)
+}
+
+impl Field {
+    pub(crate) fn get_pipe(&self, pos: (usize, usize)) -> Option<char> {
+        if pos.0 < self.width && pos.1 < self.data.len() / self.width {
+            Some(self.data[pos.1*self.width + pos.0])
+        } else {
+            None
+        }
+    }
+    pub(crate) fn get_walking_elf(&self, start: Option<(usize, usize)>) -> Elf {
+        Elf {
+            field: self,
+            pos: start.unwrap_or(self.start),
+            dir: Right,
+        }
+    }
+    pub(crate) fn parse(s: &str, start: char) -> Field {
+        let mut input = s.split('\n').peekable();
+        let width = input.peek().map(|line| line.len()).expect("Can't get field width");
+        let mut start_pos = 0;
+        let data = input.flat_map(|line| line.chars())
+            .enumerate()
+            .map(|(i,c)| {
+                if start.eq(&c) { start_pos = i; }
+                c
+            })
+            .collect::<Vec<_>>();
+
+        let start = ( start_pos % width, start_pos / width);
+
+        Field { width, data, start }
+    }
+}
