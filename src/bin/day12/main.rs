e9d200a195d9d2a3b5a1a140a183318583b5566d
gvelim <3185416+gvelim@users.noreply.github.com>
2023-12-26 11:04:52 +0000

    Day 12 - WIP file structure

--- src/bin/day12/main.rs
+++ src/bin/day12/main.rs
@@ -4,5 +4,23 @@ fn main() {
 
 #[cfg(test)]
 mod test {
+    #[test]
+    fn test_parse() {
+
+        let damaged = "#.#.###";
+        let record = [1,1,3usize];
+
+        let diter = damaged.split('.');
+        let mut riter = record.iter();
+
+        println!("{:?}",diter
+            .inspect(|d| print!("{:?}->",d) )
+            .map(|p|
+                riter.next().is_some_and(|v| p.len().eq(v))
+            )
+            .inspect(|d| println!("{:?}",d) )
+            .all(|m| m)
+        );
+    }
 
 }
