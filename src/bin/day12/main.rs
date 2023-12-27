0bb1b94b90d186ae6bbaeaef0abb4d43ba27f7f1
gvelim <3185416+gvelim@users.noreply.github.com>
2023-12-27 21:49:43 +0000

    Day 12 - WIP Combinations::parse() recursion seems to be working, needs further testing

--- src/bin/day12/main.rs
+++ src/bin/day12/main.rs
@@ -18,6 +18,10 @@ mod test {
             let mut out = vec![];
 
             println!("{:?}",(&inp,&count, inp.len(), (&count.iter().sum::<usize>() + &count.iter().count())));
+            if !inp.is_empty() && count.is_empty() {
+                println!("Abort");
+                return None;
+            }
             if inp.is_empty() && count.is_empty() {
                 println!("reached bottom!!");
                 return Some(vec![]);
@@ -26,14 +30,14 @@ mod test {
             loop {
                 match iter.next() {
                     Some('?') => {
-                        // print!("\tFork in # ->");
+                        print!("\tFork in # ->");
                         self
                             .parse(&format!("{}#{}",buf,iter.as_str()), count)
                             .inspect(|v| println!("\tFork out # {:?}",v))
                             .map(|v|
                                 v.into_iter().collect_into(&mut out)
                             );
-                        // print!("\tFork in .. ->");
+                        print!("\tFork in .. ->");
                         self
                             .parse(&format!("{}.{}",buf,iter.as_str()), count)
                             .inspect(|v| println!("\tFork out .. {:?}",v))
@@ -44,35 +48,39 @@ mod test {
                         return if out.is_empty() { None } else { Some(out) }
                     },
                     Some('.') | None if !buf.is_empty() => {
-                        if buf.trim_matches('.').len() == count[0] {
-                            println!("\t->{}",buf);
-                            return self
-                                .parse(iter.as_str(), &count[1..])
-                                .inspect(|v| println!("\tRet:{:?}",v))
-                                .map(|v| {
-                                    if buf.len() < inp.len() { buf.push('.') };
-                                    if !v.is_empty() {
-                                        v.into_iter().for_each(|s| {
-                                            out.push(buf.clone() + &s)
-                                        })
-                                    } else {
-                                        out.push(buf)
-                                    }
-                                    out
-                                })
+                        if buf.len() < inp.len() { buf.push('.') };
+
+                        if buf.contains('#') {
+                            if buf.trim_matches('.').len() == count[0]
+                            {
+                                println!("\t->{}", buf);
+                                return self
+                                    .parse(iter.as_str(), &count[1..])
+                                    .inspect(|v| println!("\tRet:{:?}", v))
+                                    .map(|v| {
+                                        if !v.is_empty() {
+                                            v.into_iter().for_each(|s| {
+                                                out.push(buf.clone() + &s)
+                                            })
+                                        } else {
+                                            out.push(buf)
+                                        }
+                                        out
+                                    })
                             } else {
                                 println!("\t Missed!");
                                 return None
                             }
+                        }
                     },
                     Some(c) => {
+                        buf.push(c);
                         let hashes = buf.chars().filter(|c| '#'.eq(c)).count();
                         println!("{hashes}::{:?}",buf);
                         if hashes > count[0] {
                             println!("abort");
                             return None
                         }
-                        buf.push(c)
                     },
                     None => return None
                 }                
@@ -87,7 +95,7 @@ mod test {
             out: vec![],
         };
 
-        let c = comb.parse(&"????.##", &[1,1,2]);
+        let c = comb.parse(&"????????##", &[1,2]);
         println!("{:?}",(c,comb.out));
     }
 }
