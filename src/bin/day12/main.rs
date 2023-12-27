bcf27c3d7cd3502b6505ae103fc305f68cb6015a
gvelim <3185416+gvelim@users.noreply.github.com>
2023-12-27 18:22:42 +0000

    park it

--- src/bin/day12/main.rs
+++ src/bin/day12/main.rs
@@ -4,8 +4,6 @@ fn main() {
 
 #[cfg(test)]
 mod test {
-    use std::iter::repeat;
-
 
     #[derive(Debug)]
     struct Combinations {
@@ -15,12 +13,9 @@ mod test {
         fn parse(&mut self, inp: &str, count: &[usize]) -> Option<String> {
             let mut string = String::new();
             let mut iter = inp.chars();
-            let mut pat = String::new();
 
             if inp.is_empty() && count.is_empty() {
                 return Some(String::new());
-            } else {
-                 pat.extend(repeat('#').take(count[0]));
             }
 
             println!("{:?}",(&inp,count));
@@ -28,16 +23,18 @@ mod test {
             loop {
                 match iter.next() {
                     Some('?') => {
-                        self.parse(&format!("{}{}{}",string,'#',iter.as_str()), count);
-                        self.parse(&format!("{}{}{}",string,'.',iter.as_str()), count);
+                        println!("#{:?}",self.parse(&format!("{}{}{}",string,'#',iter.as_str()), count));
+                        println!(".{:?}",self.parse(&format!("{}{}{}",string,'.',iter.as_str()), count));
                     }
                     Some('.') | None if !string.is_empty() => {
-                        if string.ends_with( &pat ) {
+                        if string.trim_matches('.').len() == count[0] {
+                            println!("\tGot! {:?}",(count[0], &string));
                             return self
                                 .parse(iter.as_str(), &count[1..])
-                                .map(|a| { 
-                                    println!("\tFound! {:?}",(pat, count[0], &a, &string));
-                                    a
+                                .map(|a| {
+                                    println!("\tFound! {:?}",(count[0], &a, &string));
+                                    string.push_str(&a);
+                                    string
                                 })
                         } else { 
                             println!("\tMissed! {:?}",string);
@@ -58,7 +55,7 @@ mod test {
             out: vec![],
         };
 
-        let c = comb.parse(&"??.###", &[1,3]);
-        println!("{:?}",(c,&comb));
+        let c = comb.parse(&"??.##", &[1,2]);
+        println!("{:?}",(c,comb.out));
     }
 }
