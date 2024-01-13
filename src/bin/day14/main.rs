7bf70bd90d7a722961c36ee355e89c1f1f085654
george velimachitis <3185416+gvelim@users.noreply.github.com>
2024-01-13 23:51:19 +0000

    Day 14 - Part 1 WIP passed sample test data

--- src/bin/day14/main.rs
+++ src/bin/day14/main.rs
@@ -23,31 +23,34 @@ impl ReflectorDish {
     fn to_cartesian(&self, idx:usize) -> Option<(usize,usize)> {
         if idx < self.lines * self.width { Some((idx % self.width, idx / self.lines)) } else { None }
     }
-    fn move_rock(&mut self, idx: usize, dir:Direction) -> bool {
+    fn move_rock(&mut self, idx: usize, dir:Direction) -> Option<usize> {
+        if idx >= self.layout.len() { return None }
         match dir {
-            Direction::East if idx < self.width*self.lines => Some(idx+1),
+            Direction::East if idx < self.layout.len() => Some(idx+1),
             Direction::West if idx > 0 => Some(idx - 1),
             Direction::North if idx > self.width => Some(idx - self.width),
-            Direction::South if idx < self.width*self.lines => Some(idx + self.width),
+            Direction::South if idx < self.layout.len() - self.width => Some(idx + self.width),
             _ => None
         }
-        .is_some_and(|next|{
-            if self.layout[next] == '.' {
-                self.layout.swap(idx,next);
-                self.move_rock(next,dir)
-            } else {
-                false
-            }
-        })
+            .and_then(|next|{
+                if self.layout[next] == '.' {
+                    self.layout.swap(idx,next);
+                    self.move_rock(next,dir)
+                } else {
+                    return Some(idx / self.lines)
+                }
+            })
+            .or( Some(idx / self.lines) )
     }
     fn tilt(&mut self, dir: Direction) -> usize {
         let rocks = self.round_rocks().collect::<Rc<[usize]>>();
 
         rocks.into_iter()
+            .inspect(|s| print!("idx: {s} -> "))
             .map(|&r| {
-                self.move_rock(r,dir);
-                0
+                self.move_rock(r,dir).map(|cost| self.lines - cost).unwrap()
             })
+            .inspect(|s| println!("{s}"))
             .sum::<usize>()
     }
     fn round_rocks(&self) -> impl Iterator<Item=usize> + '_ {
@@ -91,6 +94,36 @@ impl Debug for ReflectorDish {
 mod test {
     use super::*;
 
+    #[test]
+    fn test_tilt() {
+        let inp = std::fs::read_to_string("src/bin/day14/sample.txt").expect("Ops!");
+        let dish = &mut inp.parse::<ReflectorDish>().unwrap_or_default();
+
+        println!("{:?}",dish);
+        assert_eq!(dish.tilt(Direction::North),136);
+        println!("{:?}",dish);
+    }
+    #[test]
+    fn test_move_rock() {
+        let inp = std::fs::read_to_string("src/bin/day14/sample.txt").expect("Ops!");
+        let dish = &mut inp.parse::<ReflectorDish>().unwrap_or_default();
+
+        println!("{:?}",dish);
+        assert_eq!(dish.move_rock(10,Direction::North), Some(1));
+        assert_eq!(dish.move_rock(12,Direction::North), Some(0));
+        assert_eq!(dish.move_rock(13,Direction::North), Some(0));
+        assert_eq!(dish.move_rock(31,Direction::North), Some(0));
+        assert_eq!(dish.move_rock(41,Direction::North), Some(1));
+        assert_eq!(dish.move_rock(91,Direction::North), Some(2));
+        assert_eq!(dish.move_rock(92,Direction::North), Some(7));
+        assert_eq!(dish.move_rock(120,Direction::North), None);
+        println!("{:?}",dish);
+        println!("{:?}",dish.round_rocks().collect::<Rc<[_]>>());
+        assert_eq!(
+            dish.round_rocks().collect::<Rc<[_]>>(),
+            [0, 1, 2, 3, 10, 11, 21, 30, 34, 39, 47, 50, 55, 62, 66, 69, 72, 77].into()
+        )
+    }
     #[test]
     fn test_extract_rocks() {
         let inp = std::fs::read_to_string("src/bin/day14/sample.txt").expect("Ops!");
