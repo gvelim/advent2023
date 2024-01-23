0b8903ff590fdb7abbde552f749c9b894450ad61
gvelim <3185416+gvelim@users.noreply.github.com>
2024-01-23 23:21:53 +0000

    Day16 - Part 2 Pass using puzzle input

--- src/bin/day16/main.rs
+++ src/bin/day16/main.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 use std::fmt::{Debug, Formatter};
+use std::iter::repeat;
 use std::str::FromStr;
 
 type Position = usize;
@@ -7,12 +8,28 @@ type Energy = usize;
 
 fn main() {
     let inp = std::fs::read_to_string("src/bin/day16/input.txt").expect("Ops!");
-    let mut cavern = inp.parse::<Cavern>().unwrap_or_default();
+    let mut cavern = inp.parse::<Cavern>().unwrap();
 
     let t = std::time::Instant::now();
     cavern.move_beam(0, D::Right);
     println!("Part 1 : Cavern Energy = {:?} - {:?}", cavern.energy(), t.elapsed());
     assert_eq!(cavern.energy(),6902);
+
+    let t = std::time::Instant::now();
+    let m = entry_points(cavern.width,cavern.lines)
+            .map(|(idx,dir)| {
+                cavern.energise(idx,dir);
+                cavern.energy()
+            })
+            .max();
+    println!("Part 2 : Max Energy = {:?} - {:?}", m, t.elapsed());
+}
+
+fn entry_points(w:usize, h:usize) -> impl Iterator<Item=(Position, Direction)> + 'static {
+    (0..w).zip(repeat(D::Down))
+        .chain((w *(h-1)..w * h).zip(repeat(D::Up)))
+        .chain((0..w * h).step_by(w).zip(repeat(D::Right)))
+        .chain((0..w * h + 1).step_by(w).skip(1).map(|c| c-1).zip(repeat(D::Left)))
 }
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq, Hash)]
@@ -21,7 +38,7 @@ use Direction as D;
 
 impl Direction {
     fn direction(&self, tile: u8) -> Direction {
-        match (tile, *self) {
+        match (tile, self) {
             (b'/', D::Right) => D::Up,
             (b'/', D::Left) => D::Down,
             (b'/', D::Up) => D::Right,
@@ -32,16 +49,15 @@ impl Direction {
             (b'\\', D::Down) => D::Right,
             (b'-', D::Up | D::Down) => D::LeftRight,
             (b'|' , D::Right | D::Left) => D::UpDown,
-            (_,d) => d
+            (_,d) => *d
         }
     }
 }
 
-#[derive(Default)]
 pub(crate) struct Cavern {
     width: usize,
     lines: usize,
-    con: Vec<u8>,
+    con: std::rc::Rc<[u8]>,
     nrg: Vec<u8>,
     tail: HashMap<usize,Direction>
 }
@@ -50,7 +66,11 @@ impl Cavern {
     fn energy(&self) -> Energy {
         self.nrg.iter().filter(|c| b'#'.eq(c)).count()
     }
-
+    fn energise(&mut self, idx: Position, dir:Direction) -> Option<Energy> {
+        self.tail.clear();
+        self.nrg.fill(b'.');
+        self.move_beam(idx,dir)
+    }
     fn step(&self, idx: Position, dir:Direction) -> Option<Position> {
         match dir {
             D::Right if idx % self.width < self.width-1 => Some(idx + 1),
@@ -60,7 +80,6 @@ impl Cavern {
             _ => None
         }
     }
-
     fn move_beam(&mut self, idx: Position, dir:Direction) -> Option<Energy> {
 
         // Has the bean hit a contraption
@@ -81,8 +100,7 @@ impl Cavern {
         // Find new direction based on current tile
         match dir.direction(self.con[idx]) {
             d@(D::Right | D::Left | D::Up | D::Down) =>
-                self.step(idx, d).and_then(|next| self.move_beam(next, d))
-            ,
+                self.step(idx, d).and_then(|next| self.move_beam(next, d)),
             D::LeftRight =>
                 Some(
                     self.step(idx, D::Left).and_then(|next| self.move_beam(next, D::Left)).unwrap_or(0)
@@ -107,7 +125,7 @@ impl FromStr for Cavern {
             lines: s.lines().count(),
             con: s.lines()
                 .flat_map(|line| line.bytes())
-                .collect::<Vec<_>>(),
+                .collect::<std::rc::Rc<[u8]>>(),
             nrg: vec![b'.'; s.len()],
             tail: HashMap::new()
         })
@@ -116,15 +134,17 @@ impl FromStr for Cavern {
 
 impl Debug for Cavern {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f,"Cavern\n")?;
+        writeln!(f,"Cavern")?;
         writeln!(f,"Width:{}, Length:{}", self.width, self.lines)?;
+        writeln!(f,"{a:w$}  {b:w$}", w=self.width<<1, a = "Contraption", b = "Energy")?;
+        writeln!(f,"{a:-<w$}  {a:-<w$}", w=self.width<<1, a="")?;
 
         let mut citer = self.con.iter();
         let mut eiter = self.nrg.iter();
         for _ in 0..self.lines {
-            for _ in 0..self.width { write!(f, "{} ", *citer.next().unwrap() as char)? };
+            for _ in 0..self.width { write!(f, "{:2}", *citer.next().unwrap() as char)? };
             write!(f, "  ")?;
-            for _ in 0..self.width { write!(f, "{} ", *eiter.next().unwrap() as char)? };
+            for _ in 0..self.width { write!(f, "{:2}", *eiter.next().unwrap() as char)? };
             writeln!(f)?;
         }
         Ok(())
@@ -132,12 +152,28 @@ impl Debug for Cavern {
 }
 #[cfg(test)]
 mod test {
-    use crate::{Cavern, D};
+    use super::*;
 
+    #[test]
+    fn test_find_max_energy() {
+        let inp = std::fs::read_to_string("src/bin/day16/sample.txt").expect("Ops!");
+        let mut cavern = inp.parse::<Cavern>().unwrap();
+
+        let m = entry_points(cavern.width,cavern.lines)
+            .inspect(|d| print!("{:?} -> ",d))
+            .map(|(idx,dir)| {
+                cavern.energise(idx,dir);
+                // println!("{:?}",cavern);
+                cavern.energy()
+            })
+            .inspect(|d| println!("{d:2}"))
+            .max();
+        assert_eq!(m,Some(51));
+    }
     #[test]
     fn test_move_bean() {
         let inp = std::fs::read_to_string("src/bin/day16/sample.txt").expect("Ops!");
-        let mut cavern = inp.parse::<Cavern>().unwrap_or_default();
+        let mut cavern = inp.parse::<Cavern>().unwrap();
 
         println!("{:?}",cavern.move_beam(0,D::Right));
         println!("{:?}",cavern);
@@ -147,7 +183,7 @@ mod test {
     #[test]
     fn test_parse_cavern() {
         let inp = std::fs::read_to_string("src/bin/day16/sample.txt").expect("Ops!");
-        let cavern = inp.parse::<Cavern>().unwrap_or_default();
+        let cavern = inp.parse::<Cavern>().unwrap();
         println!("{:?}",cavern);
     }
 }
