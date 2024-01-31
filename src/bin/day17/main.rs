47554dc227f87ffe781c20e3b66f11628fbed259
gvelim <3185416+gvelim@users.noreply.github.com>
2024-01-31 22:16:09 +0000

    Day17 - Part 1 WIP DP min path excl. 3 steps constraint

--- src/bin/day17/main.rs
+++ src/bin/day17/main.rs
@@ -15,7 +15,7 @@ impl FromStr for CityMap {
         Ok(CityMap {
             map: s
                 .lines()
-                .map(|line| line.bytes().map(|c| c - b'0').collect::<Rc<[u8]>>())
+                .map(|line| line.bytes().map(|c| c - b'0').collect::<Rc<[_]>>())
                 .collect::<Rc<_>>(),
         })
     }
@@ -24,7 +24,8 @@ impl FromStr for CityMap {
 impl Debug for CityMap {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         self.map.iter().for_each(|line| {
-            writeln!(f, "{:?}", line).expect("Ops");
+            line.iter().for_each(|c| write!(f, "{:3}", c).expect("Ops"));
+            writeln!(f).expect("");
         });
         Ok(())
     }
@@ -33,6 +34,27 @@ impl Debug for CityMap {
 #[cfg(test)]
 mod test {
     use crate::CityMap;
+
+    #[test]
+    fn test_calc_min_path() {
+        let input = std::fs::read_to_string("src/bin/day17/sample.txt").expect("File Not Found!");
+        let map = input.parse::<CityMap>().expect("ops");
+
+        let mut dp = vec![vec![u8::MAX; map.map[0].len()+1]; map.map.len()+1];
+        dp[0][1] = 0;
+        dp[1][0] = 0;
+
+        (1..dp.len()).for_each(|y|
+                (1..dp[0].len()).for_each(|x|
+                    dp[x][y] = std::cmp::min(dp[x-1][y], dp[x][y-1]) + map.map[x-1][y-1]
+                )
+        );
+
+        dp.iter().for_each(|line| {
+            line.iter().for_each(|c| print!("{:4}", c));
+            println!();
+        });
+    }
 
     #[test]
     fn test_parse_map() {
