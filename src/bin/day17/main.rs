a6595a18c4eebe3b09e885c1401f1abd73f94ba3
george velimachitis <3185416+gvelim@users.noreply.github.com>
2024-02-20 22:01:51 +0200

    Day17 - refactor & minor bug fix

--- src/bin/day17/main.rs
+++ src/bin/day17/main.rs
@@ -1,5 +1,9 @@
-use crate::citymap::CityMap;
-use crate::direction::Direction as D;
+use std::ops::Range;
+use crate::{
+    block::{Heat, Position},
+    citymap::CityMap,
+    direction::Direction as D
+};
 
 mod citymap;
 mod direction;
@@ -11,16 +15,19 @@ fn main() {
     let input = std::fs::read_to_string("src/bin/day17/input.txt").expect("File Not Found!");
     let map = input.parse::<CityMap>().expect("ops");
 
-    let mut c = map.get_crucible(0, D::Right);
-    let t = std::time::Instant::now();
-    let path = c.find_path_to(map.len()-1, 1..3);
-    assert_eq!(path.as_ref().unwrap().total_heat_loss(), 1008);
-    println!("Part 1: {:?} = {:?}", path.unwrap().total_heat_loss(), t.elapsed());
+    let total_heat_loss = |rng: Range<Position>| -> Option<Heat> {
+        map.get_crucible(0, D::Right)
+            .find_path_to(map.len()-1, rng)
+            .map(|path| path.total_heat_loss() )
+    };
 
-
-    let mut c = map.get_crucible(0, D::Right);
     let t = std::time::Instant::now();
-    let path = c.find_path_to(map.len()-1, 4..10);
-    assert_eq!(path.as_ref().unwrap().total_heat_loss(), 1210);
-    println!("Part 2: {:?} = {:?}", &path.unwrap().total_heat_loss(), t.elapsed());
+    let loss = total_heat_loss(0..3);
+    println!("Part 1: {:?} = {:?}", loss, t.elapsed());
+    assert_eq!(loss, Some(1008), "Part 1 result doesn't match");
+
+    let t = std::time::Instant::now();
+    let loss = total_heat_loss(4..10);
+    println!("Part 2: {:?} = {:?}", loss, t.elapsed());
+    assert_eq!(loss, Some(1210), "Part 2 result doesn't match");
 }
