ccc851ad6d39be1bf618ee674216eed77eb689f9
gvelim <3185416+gvelim@users.noreply.github.com>
2024-02-24 14:09:19 +0000

    Day18 - WIP parsing  InstructionSet

--- src/bin/day18/main.rs
+++ src/bin/day18/main.rs
@@ -1,13 +1,32 @@
-use std::{fmt::Debug, num::ParseIntError, ops::Shl, str::FromStr};
+use std::{fmt::Debug, str::FromStr};
 
 fn main() {
 
 }
 
+#[derive(Debug)]
+struct InstructionSet {
+    set: std::rc::Rc<[Instruction]>
+}
+
+impl FromStr for InstructionSet {
+    type Err = InstructionErr;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut set = vec![];
+        for line in s.lines() {
+            set.push( line.parse::<Instruction>()?);
+        }
+        Ok(InstructionSet{
+            set: set.into()
+        })
+    }
+}
+
 #[derive(Debug,PartialEq)]
 enum Direction { U, R, D, L}
 
-#[derive(Debug,PartialEq)]
+#[derive(PartialEq)]
 struct Instruction {
     dir: Direction, 
     run: usize,
@@ -20,7 +39,12 @@ enum InstructionErr {
     InvalidRunLength,
     InvalidRGB
 }
-
+impl Debug for Instruction {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?} {} (#{:02x}{:02x}{:02x})",self.dir,self.run,self.rgb.0,self.rgb.1,self.rgb.2)?;
+        Ok(())
+    }
+}
 impl Debug for InstructionErr {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
@@ -66,13 +90,31 @@ mod test {
     use super::*;
 
     #[test]
+    fn test_instructionset_parse()  {
+        let inp = std::fs::read_to_string("./src/bin/day18/sample.txt").expect("Ops!");
+        let set = match inp.parse::<InstructionSet>() {
+            Ok(set) => set,
+            Err(e) => panic!("{:?}",e),
+        };
+    
+        set.set.iter().for_each(|ins| println!("{:?}",ins) );
+        assert_eq!(set.set.len(), 14);
+    }
+
+    #[test]
     fn test_instruction_parse() {
         let data = [
             ("R 6 (#4d17d2)", Ok(Instruction { dir: Direction::R, run: 6, rgb: (0x4d,0x17,0xD2)})),
-            ("K 5 (#af8603)",Err(InstructionErr::InvalidDirection)),
-            ("L a (#1a3700)",Err(InstructionErr::InvalidRunLength)),
+            ("U 10 (4d17d2)", Ok(Instruction { dir: Direction::U, run: 10, rgb: (0x4d,0x17,0xD2)})),
+            ("U 10 #4d17d2", Ok(Instruction { dir: Direction::U, run: 10, rgb: (0x4d,0x17,0xD2)})),
+            ("U 10 4d17d2", Ok(Instruction { dir: Direction::U, run: 10, rgb: (0x4d,0x17,0xD2)})),
+            ("K 5 (#af8603)", Err(InstructionErr::InvalidDirection)),
+            ("L a (#1a3700)", Err(InstructionErr::InvalidRunLength)),
             ("U 10 (#6534071)", Err(InstructionErr::InvalidRGB)),
             ("U 10 (#65L071)", Err(InstructionErr::InvalidRGB)),
+            ("U 10 (#G071)", Err(InstructionErr::InvalidRGB)),
+            ("U 10 [#4d17d2]", Err(InstructionErr::InvalidRGB)),
+            ("U 10 (*4d17d2)", Err(InstructionErr::InvalidRGB)),
         ];
 
         for (inp,out) in data {
