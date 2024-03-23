a9b9254d7fbfe857ea6c66b3004ba0e2c454b306
gvelim <3185416+gvelim@users.noreply.github.com>
2024-03-23 08:38:52 +0000

    Day19 - part 1 WIP
    parsing combination tree
    refactor rule so part variable can be assessed

--- src/bin/day19/rule.rs
+++ src/bin/day19/rule.rs
@@ -1,39 +1,59 @@
-use std::{fmt::Debug, num::ParseIntError, rc::Rc, str::FromStr};
 use crate::part::{Part, Unit};
+use std::{fmt::Debug, num::ParseIntError, ops::Range, rc::Rc, str::FromStr};
 
-enum Operant { XGT, XLT, MGT, MLT, SGT, SLT, AGT, ALT }
+#[derive(Clone, Copy)]
+pub(crate) enum PartVar {
+    X = 0,
+    M,
+    A,
+    S,
+}
 
-impl Debug for Operant {
+impl Debug for PartVar {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Operant::XGT => write!(f,"x>"),
-            Operant::XLT => write!(f,"x<"),
-            Operant::MGT => write!(f,"m>"),
-            Operant::MLT => write!(f,"m<"),
-            Operant::SGT => write!(f,"s>"),
-            Operant::SLT => write!(f,"s<"),
-            Operant::AGT => write!(f,"a>"),
-            Operant::ALT => write!(f,"a<"),
+            PartVar::X => write!(f, "x"),
+            PartVar::M => write!(f, "m"),
+            PartVar::A => write!(f, "a"),
+            PartVar::S => write!(f, "s"),
         }
     }
 }
 
-struct Condition {
+enum Operant {
+    GT,
+    LT,
+}
+
+impl Debug for Operant {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Operant::GT => write!(f, ">"),
+            Operant::LT => write!(f, "<"),
+        }
+    }
+}
+
+pub(crate) struct Condition {
+    var: PartVar,
     operant: Operant,
-    value: Unit
+    value: Unit,
 }
 
 impl Condition {
+    pub(crate) fn part(&self) -> PartVar {
+        self.var
+    }
     fn validate(&self, part: Part) -> bool {
-        match self.operant {
-            Operant::XGT => part.x > self.value,
-            Operant::XLT => part.x < self.value,
-            Operant::MGT => part.m > self.value,
-            Operant::MLT => part.m < self.value,
-            Operant::SGT => part.s > self.value,
-            Operant::SLT => part.s < self.value,
-            Operant::AGT => part.a > self.value,
-            Operant::ALT => part.a < self.value,
+        match (&self.var, &self.operant) {
+            (PartVar::X, Operant::GT) => part.x > self.value,
+            (PartVar::X, Operant::LT) => part.x < self.value,
+            (PartVar::M, Operant::GT) => part.m > self.value,
+            (PartVar::M, Operant::LT) => part.m < self.value,
+            (PartVar::S, Operant::GT) => part.s > self.value,
+            (PartVar::S, Operant::LT) => part.s < self.value,
+            (PartVar::A, Operant::GT) => part.a > self.value,
+            (PartVar::A, Operant::LT) => part.a < self.value,
         }
     }
 }
@@ -43,25 +63,26 @@ impl FromStr for Condition {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         Ok(Condition {
-            operant: match &s[..2] {
-                "x>" => Operant::XGT,
-                "x<" => Operant::XLT,
-                "m>" => Operant::MGT,
-                "m<" => Operant::MLT,
-                "s>" => Operant::SGT,
-                "s<" => Operant::SLT,
-                "a>" => Operant::AGT,
-                "a<" => Operant::ALT,
-                _ => panic!("Condition::operant::from_str(): invalid operand")
+            var: match &s[..1] {
+                "x" => PartVar::X,
+                "m" => PartVar::M,
+                "a" => PartVar::A,
+                "s" => PartVar::S,
+                _ => panic!("Condition::operant::from_str(): invalid part variable"),
             },
-            value: Unit::from_str(&s[2..]).expect("Condition::value::from_str(): invalid number")
+            operant: match &s[1..2] {
+                ">" => Operant::GT,
+                "<" => Operant::LT,
+                _ => panic!("Condition::operant::from_str(): invalid operand"),
+            },
+            value: Unit::from_str(&s[2..]).expect("Condition::value::from_str(): invalid number"),
         })
     }
 }
 
 impl Debug for Condition {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:?}{:?}",self.operant,self.value)
+        write!(f, "{:?}{:?}{:?}", self.var, self.operant, self.value)
     }
 }
 
@@ -69,7 +90,7 @@ impl Debug for Condition {
 pub(crate) enum Action {
     WorkFlow(Rc<str>),
     Accept,
-    Reject
+    Reject,
 }
 
 impl FromStr for Action {
@@ -79,7 +100,7 @@ impl FromStr for Action {
         Ok(match s {
             "A" => Self::Accept,
             "R" => Self::Reject,
-            wf => Self::WorkFlow(wf.into())
+            wf => Self::WorkFlow(wf.into()),
         })
     }
 }
@@ -87,9 +108,9 @@ impl FromStr for Action {
 impl Debug for Action {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Action::WorkFlow(s) => write!(f,"{}",s),
-            Action::Accept => write!(f,"A"),
-            Action::Reject => write!(f,"R"),
+            Action::WorkFlow(s) => write!(f, "{}", s),
+            Action::Accept => write!(f, "A"),
+            Action::Reject => write!(f, "R"),
         }
     }
 }
@@ -98,7 +119,7 @@ pub(crate) enum Rule {
     // each rule specifies a condition and where to send the part if the condition is true
     // The last rule in each workflow has no condition and always applies if reached.
     ConAct(Condition, Action),
-    Act(Action)
+    Act(Action),
 }
 
 impl Rule {
@@ -110,6 +131,46 @@ impl Rule {
         }
     }
 
+    pub(crate) fn validate_range(
+        &self,
+        rng: &Range<Unit>,
+    ) -> Option<(Range<Unit>, Option<Rc<str>>)> {
+        match self {
+            Rule::ConAct(c, a) => {
+                if rng.contains(&c.value) {
+                    let r = match c.operant {
+                        Operant::GT => {
+                            if a == &Action::Reject {
+                                rng.start..c.value
+                            } else {
+                                c.value..rng.end
+                            }
+                        }
+                        Operant::LT => {
+                            if a == &Action::Reject {
+                                c.value..rng.end
+                            } else {
+                                rng.start..c.value
+                            }
+                        }
+                    };
+                    Some((
+                        r,
+                        if let Action::WorkFlow(w) = a {
+                            Some(w.clone())
+                        } else {
+                            None
+                        },
+                    ))
+                } else {
+                    None
+                }
+            }
+            Rule::Act(Action::WorkFlow(wf)) => Some((rng.clone(), Some(wf.clone()))),
+            Rule::Act(Action::Accept) => Some((rng.clone(), None)),
+            Rule::Act(Action::Reject) => None,
+        }
+    }
 }
 
 impl FromStr for Rule {
@@ -119,14 +180,16 @@ impl FromStr for Rule {
         // x>10:one, m<20:two, a>30:R, A
         let mut s = s.split(':');
         let o = match (s.next(), s.next()) {
-            (Some(s), None) =>
-                Self::Act(s.parse::<Action>().expect("Rule::Act::Action::from_str failed")),
-            (Some(op), Some(res)) => {
-                Self::ConAct(
-                    op.parse::<Condition>().expect("Rule::ConAct::Condition::from_str failed"),
-                    res.parse::<Action>().expect("Rule::ConAct::Action::from_str failed")
-                )
-            },
+            (Some(s), None) => Self::Act(
+                s.parse::<Action>()
+                    .expect("Rule::Act::Action::from_str failed"),
+            ),
+            (Some(op), Some(res)) => Self::ConAct(
+                op.parse::<Condition>()
+                    .expect("Rule::ConAct::Condition::from_str failed"),
+                res.parse::<Action>()
+                    .expect("Rule::ConAct::Action::from_str failed"),
+            ),
             _ => return Err(()),
         };
         Ok(o)
@@ -136,18 +199,52 @@ impl FromStr for Rule {
 impl Debug for Rule {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Rule::ConAct(c, r) => write!(f, "{:?}:{:?}",c,r),
-            Rule::Act(r) => write!(f,"{:?}",r),
+            Rule::ConAct(c, r) => write!(f, "{:?}:{:?}", c, r),
+            Rule::Act(r) => write!(f, "{:?}", r),
         }
     }
 }
-
-
 
 #[cfg(test)]
 mod test {
     use super::*;
     use crate::workflow::Workflow;
+
+    #[test]
+    fn test_rule_check_range() {
+        let data = [
+            // accept cases / in-bound / conditional
+            "x>5:A".parse::<Rule>().unwrap(),
+            "m<35:A".parse::<Rule>().unwrap(),
+            // reject cases / in-bound / conditional
+            "a>30:R".parse::<Rule>().unwrap(),
+            "a<10:R".parse::<Rule>().unwrap(),
+            // accept cases / off-bound / conditional
+            "a<5:A".parse::<Rule>().unwrap(),
+            "m>35:A".parse::<Rule>().unwrap(),
+            // wf cases / in-bound / conditional
+            "m>15:qzz".parse::<Rule>().unwrap(),
+            "m<25:hjg".parse::<Rule>().unwrap(),
+            // wf cases / off-bound / conditional
+            "m<10:qzz".parse::<Rule>().unwrap(),
+            "m>30:hjg".parse::<Rule>().unwrap(),
+            // non-conditional cases
+            "qzz".parse::<Rule>().unwrap(),
+            "A".parse::<Rule>().unwrap(),
+            "R".parse::<Rule>().unwrap(),
+        ];
+        let mut rng = 0..40;
+        for r in data {
+            print!("rule: {:?}, range:{:?} ==> ", r, rng);
+            match r.validate_range(&rng) {
+                Some((r, wf)) => {
+                    rng = r;
+                    println!("{:?}", (&rng, wf));
+                }
+                None => println!("Rejected"),
+            }
+        }
+    }
 
     #[test]
     fn test_rule_validate() {
@@ -158,30 +255,35 @@ mod test {
             None,
             Some(Action::Reject),
             None,
-            Some(Action::Accept)
+            Some(Action::Accept),
         ]
         .into_iter();
-        let wf = "ex{x>10:one,x<10:one,m<20:two,m>20:two,a<30:R,a>30:R,A}".parse::<Workflow>().expect("Ops");
-        let part = Part{ x: 11, m: 0, a: 20, s: 0 };
+        let wf = "ex{x>10:one,x<10:one,m<20:two,m>20:two,a<30:R,a>30:R,A}"
+            .parse::<Workflow>()
+            .expect("Ops");
+        let part = Part {
+            x: 11,
+            m: 0,
+            a: 20,
+            s: 0,
+        };
 
-        wf.iter()
-            .for_each(|rule|{
-                println!("{:?} => {:?} = {:?}", rule, part, rule.validate(part));
-                assert_eq!(
-                    format!("{:?}", res.next().unwrap()),
-                    format!("{:?}", rule.validate(part))
-                );
-            });
+        wf.iter().for_each(|rule| {
+            println!("{:?} => {:?} = {:?}", rule, part, rule.validate(part));
+            assert_eq!(
+                format!("{:?}", res.next().unwrap()),
+                format!("{:?}", rule.validate(part))
+            );
+        });
     }
 
     #[test]
     fn test_rule_parse() {
         let inp = "x>10:one\nm<20:two\na>30:R\nA";
-        inp.lines()
-            .for_each(|s| {
-                let r = s.parse::<Rule>().expect("Rule::parse() error!");
-                println!("{:?}",r);
-                assert_eq!(&format!("{:?}",r),&s)
-            })
+        inp.lines().for_each(|s| {
+            let r = s.parse::<Rule>().expect("Rule::parse() error!");
+            println!("{:?}", r);
+            assert_eq!(&format!("{:?}", r), &s)
+        })
     }
 }
--- src/bin/day19/system.rs
+++ src/bin/day19/system.rs
@@ -1,9 +1,14 @@
-use std::{collections::HashMap, rc::Rc, str::FromStr};
+use crate::part::Unit;
 use crate::workflow::Workflow;
-use crate::{part::Part, rule::Action};
+use crate::{
+    part::Part,
+    rule::{Action, Rule},
+};
+use std::ops::Range;
+use std::{collections::HashMap, rc::Rc, str::FromStr};
 
 pub(crate) struct SortingSystem {
-    map: HashMap<Rc<str>, Workflow>
+    map: HashMap<Rc<str>, Workflow>,
 }
 
 impl SortingSystem {
@@ -23,9 +28,35 @@ impl SortingSystem {
                 .expect("SortingSystem::process() - redirected to non-existent Workflow");
             print!("{:?} -> ", wf.name);
         }
+        let out = wf.validate(part);
+        println!("{:?}", out.as_ref().unwrap());
+        out
+    }
 
-        println!("{:?}", &wf.validate(part));
-        wf.validate(part)
+    pub(crate) fn rating_combinations(&self, wf: &str, rngs: &[Range<Unit>; 4]) -> Option<[Range<Unit>; 4]> {
+        self.map
+            .get(wf.into())
+            .unwrap()
+            .iter()
+            // .inspect(|arr| println!("{:?} -> ", arr))
+            .filter_map(|rule| match rule {
+                Rule::ConAct(c, a) => {
+                    let mut nrng = rngs.clone();
+                    if let Some((drng, _)) = rule.validate_range(&rngs[c.part() as usize]) {
+                        nrng[c.part() as usize] = drng;
+                    }
+                    match a {
+                        Action::WorkFlow(wf) => self.rating_combinations(wf, &nrng),
+                        Action::Accept => Some(nrng),
+                        Action::Reject => Some(nrng),
+                    }
+                }
+                Rule::Act(Action::WorkFlow(wf)) => self.rating_combinations(wf, rngs),
+                Rule::Act(Action::Accept) => Some(rngs.clone()),
+                Rule::Act(Action::Reject) => None,
+            })
+            .inspect(|arr| println!(" = {:?}", arr))
+            .last()
     }
 }
 
@@ -33,7 +64,7 @@ impl FromStr for SortingSystem {
     type Err = ();
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut map: HashMap<Rc<str>,Workflow> = HashMap::new();
+        let mut map: HashMap<Rc<str>, Workflow> = HashMap::new();
 
         for line in s.lines() {
             let wf = line.parse::<Workflow>()?;
@@ -46,7 +77,13 @@ impl FromStr for SortingSystem {
 #[cfg(test)]
 mod test {
     use super::*;
-    use crate::{parse_puzzle_data, part::Unit};
+    use crate::parse_puzzle_data;
+
+    #[test]
+    fn test_sortingsystem_combinations() {
+        let (_, wfs) = parse_puzzle_data("src/bin/day19/sample.txt");
+        wfs.rating_combinations("in", &[0..4000, 0..4000, 0..4000, 0..4000]);
+    }
 
     #[test]
     fn test_sortingsystem_process() {
@@ -63,21 +100,18 @@ mod test {
 
     #[test]
     fn test_sortingsystem_parse() {
-        let inp = std::fs::read_to_string("src/bin/day19/sample.txt")
-            .expect("cannot load sample.txt");
+        let inp =
+            std::fs::read_to_string("src/bin/day19/sample.txt").expect("cannot load sample.txt");
         let wfs = inp.split("\n\n").next().unwrap();
-        let sorting = wfs.parse::<SortingSystem>()
+        let sorting = wfs
+            .parse::<SortingSystem>()
             .expect("Failed to parse workflow");
 
-        wfs.lines()
-            .for_each(|line|{
-                let wf = line.parse::<Workflow>().expect("msg");
-                let found = sorting.map.get(&wf.key());
-                println!("{:?}",found);
-                assert_eq!(
-                    format!("{:?}", found),
-                    format!("{:?}", Some(wf))
-                );
-            });
+        wfs.lines().for_each(|line| {
+            let wf = line.parse::<Workflow>().expect("msg");
+            let found = sorting.map.get(&wf.key());
+            println!("{:?}", found);
+            assert_eq!(format!("{:?}", found), format!("{:?}", Some(wf)));
+        });
     }
 }
